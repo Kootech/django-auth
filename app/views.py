6c0b3ab4ba3246ba8a5e910f0b3114f7b358e4c5
kelvin <kitetesh.km@gmail.com>
2022-02-23 12:22:04 +0300

    :q
    second commit

--- app/views.py
+++ app/views.py
@@ -1,11 +1,31 @@
 from email.headerregistry import Group
 from django.shortcuts import redirect, render
+from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+# decorator to allow only a certain group of users
+def allowedUsers(allowed_groups=[]):
+    def decoratorFunc(viewFunc):
+        def wrapper(request, *args, **kwargs):
+            try:
+                group = request.user.groups.all()[0].name
+
+            except:
+                return
+            if group in allowed_groups:
+                return viewFunc(request, *args, **kwargs)
+            else:
+                return HttpResponse('not allowed')
+        return wrapper
+    return decoratorFunc
+            
+
 
 def loginPage(request):
     if request.method == 'POST':
@@ -47,6 +67,7 @@ def registerPage(request):
 
 
 @login_required(login_url='login')
+@allowedUsers(allowed_groups='admin')
 def dashboard(request):
     return render(request, 'app/dashboard.html')
 
